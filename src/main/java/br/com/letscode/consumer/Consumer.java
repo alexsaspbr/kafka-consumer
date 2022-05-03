75d6361b28359bd380c1a0f8357e2bcc202c9478
alexsaspbr <alexaraujo@Avell.localdomain>
2022-05-03 00:14:27 -0300

    create kafka consumer

---
+++ src/main/java/br/com/letscode/consumer/Consumer.java
@@ -0,0 +1,17 @@
+package br.com.letscode.consumer;
+
+import br.com.letscode.domain.CardDTO;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @KafkaListener(groupId = "consumer-id", topics = "${topic}")
+    public void consume(CardDTO cardDTO) {
+
+        System.out.println(cardDTO.getName());
+
+    }
+
+}
---
+++ src/main/java/br/com/letscode/domain/CardDTO.java
@@ -0,0 +1,14 @@
+package br.com.letscode.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CardDTO {
+
+    private String pan;
+    private String name;
+    private Brand brand;
+
+}
